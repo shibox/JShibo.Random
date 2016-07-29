c16e2fe0d13d65b3e4e18fdc473dcf58acd5d149
shibox <jarvey.create@163.com>
2016-07-29 18:52:10 +0800

    创建

--- JShibo.Random.Tests/Program.cs
+++ JShibo.Random.Tests/Program.cs
@@ -14,15 +14,18 @@ namespace JShibo.Random.Tests
             int size = 10000000;
             ShiboRandom rd = new ShiboRandom();
             System.Random r = new System.Random();
-            int[] array = new int[size];
+            uint[] array = new uint[size];
             Stopwatch w = Stopwatch.StartNew();
-            for (int i = 0; i < size; i++)
+            for (int i = 0; i < 10; i++)
             {
                 //r.NextDouble();
                 //rd.NextDouble();
                 //rd.NextFloat();
 
-                array[i] = rd.Next(-10000);
+                //array[i] = rd.Next(-10000);
+
+                //rd.NextUInts(array, 0, array.Length);
+                rd.NextUInts8(array, 0, array.Length);
             }
             w.Stop();
             Console.WriteLine(w.ElapsedMilliseconds);
